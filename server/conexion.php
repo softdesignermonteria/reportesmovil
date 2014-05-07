c6cf538aa2107ed1f986154e826bcc3745fc10b0
softdesignermonteria <administrador@softdesignermonteria.net>
2014-05-07 17:52:51 -0500

    actualizacion de servidor

--- server/conexion.php
+++ server/conexion.php
@@ -4,7 +4,7 @@
 	$usuario="root";
 	$clave="mysqlsdmnet2013*";
 	$bd="factutesis";
-    $conexion = mysql_connect($host, $usuario, $clave);
+    $conexion = @mysql_connect($host, $usuario, $clave);
     mysql_select_db($bd, $conexion) or die("Error al conectar a la BD: ".$bd);
 	
 ?>
