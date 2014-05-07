81e99f0fb61a85cd11b0aa76af9b95240b8e58c9
softdesignermonteria <administrador@softdesignermonteria.net>
2014-05-07 16:54:46 -0500

    subiendo reportes movil para phonegap

--- server/conexion.php
+++ server/conexion.php
@@ -2,7 +2,7 @@
 	
 	$host="localhost";
 	$usuario="root";
-	$clave="123";
+	$clave="mysqlsdmnet2013*";
 	$bd="factutesis";
     $conexion = mysql_connect($host, $usuario, $clave);
     mysql_select_db($bd, $conexion) or die("Error al conectar a la BD: ".$bd);
